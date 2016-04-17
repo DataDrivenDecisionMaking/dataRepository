bb38625645daa74bec936b4714b08ff5e4053b81
mrsingh <mrsingh@ncsu.edu>
2016-04-17 17:48:02 -0400

    incremental commit

---
+++ SQL_SCRIPTS/weight_table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE weight_table(
+
+v_customer_id varchar2(100),
+v_plan_id varchar2(25),
+v_plan_marketing_name varchar2(25),
+v_premium number,
+v_deductible number,
+v_service_score number,
+v_cost_score number,
+v_final_score number
+)
